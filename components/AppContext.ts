72bdf078ebb57413b97e91a4aae412a4a9b0d77a
diberry <diberry@microsoft.com>
2019-12-31 17:10:35 -0800

    apps list returned - bare bones - no tests

---
+++ components/AppContext.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ILuisApp } from 'dfb-luis-apps-lib';
+
+type ContextProps = {
+    apps: ILuisApp[],
+    key: string,
+    endpoint: string
+}
+
+const AppContext = React.createContext<Partial<ContextProps>>({});
+
+export { AppContext };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,39 @@
-const Home = () => <h1>Hello world!</h1>;
+import { NextPage } from 'next';
+
+import { ILuisApp, LuisApps, IValues, IFeatureFlags} from 'dfb-luis-apps-lib';
+
+const Home: NextPage<{ userAgent: string, apps: string}> = ({ userAgent, apps}) => {
+  return (
+  <div>
+    <h1>Hello world! - user agent: {userAgent}</h1>
+    <div>{apps}</div>
+  </div>
+  )
+};
+
+Home.getInitialProps = async ({ req }) => {
+
+  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+
+  const key = '';
+  const endpoint = 'https://diberry-luis-free-f0-s0-authoring.cognitiveservices.azure.com/';
+
+  const values: IValues = {
+    endpoint: endpoint,
+    key: key,
+  };
+  const features: IFeatureFlags = {
+    versions: false,
+    models: false,
+  };
+
+  const appsReturned: ILuisApp[] = await LuisApps.getApps(values, features);
+
+  const apps = JSON.stringify(appsReturned);
+  console.log(apps);
+
+  return { userAgent, apps };
+};
 
 export default Home;
+
